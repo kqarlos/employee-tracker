fb85a0582d080f2abaa9ce2b7908fb2023c6a3f1
kqarlos <kqarlos@hotmail.com>
2020-01-16 12:54:03 -0800

    Updates general workflow of the application

--- employeeTracker.js
+++ employeeTracker.js
@@ -1,6 +1,28 @@
 var mysql = require("mysql");
 var inquirer = require("inquirer");
 
+var menu = {
+    type: "list",
+    name: "menuChoice",
+    message: "What would you like to do?",
+    choices: [
+        "View All Employees",
+        // "View All Employees by Department",
+        // "View All Employees by Manager",
+        "Add Employee",
+        // "Remove Employee",
+        "Update Employee Role",
+        // "Update Employee Manager",
+        "View All Roles",
+        "Add Role",
+        // "Remove Role",
+        "View All Departments",
+        "Add Department",
+        "Exit"
+        // "Remove Department",
+        // "View Department Budget"
+    ]
+}
 //create connection information to sql database
 var connection = mysql.createConnection({
     host: "localhost",
@@ -14,11 +36,154 @@ var connection = mysql.createConnection({
 connection.connect(function (err) {
     if (err) throw err;
     console.log("Connected as id: " + connection.threadId + "\n");
-    start();
+    mainMenu();
 });
 
-//start application
-function start() {
-    console.log("Closing connection...");
-    connection.end();
+//start application prompt
+function mainMenu() {
+    inquirer.prompt(menu).then(function (res) {
+        switch (res.menuChoice) {
+            case "View All Employees":
+                qGetEmployees(displayEmployees);
+                break;
+            case "View All Employees by Department":
+                break;
+            case "View All Employees by Manager":
+                break;
+            case "Add Employee":
+                promptForEmployeeinfo();
+                break;
+            case "Remove Employee":
+                break;
+            case "Update Employee Role":
+                qGetEmployees(promptSelectEmployees);
+                break;
+            case "Update Employee Manager":
+                break;
+            case "View All Roles":
+                qGetRoles(displayRoles);
+                break;
+            case "Add Role":
+                promptRoleInfo();
+                break;
+            case "Remove Role":
+                break;
+            case "View All Departments":
+                qGetDepartments();
+                break;
+            case "Add Department":
+                promptDepartmentInfo();
+                break;
+            case "Remove Department":
+                break;
+            case "View Department Budget":
+                break;
+            case "Exit":
+                console.log("Closing connection...");
+                connection.end();
+                break;
+        }
+    });
 }
+
+//QUERIES ===================================
+
+//queries all employees
+function qGetEmployees(cb) {
+    console.log("Querying all employees...");
+    cb();
+}
+
+//queries to add employee
+function qAddEmployee() {
+    console.log("Querying: add employee...");
+    mainMenu();
+}
+
+//queries to update employee role
+function qUpdateEmployeeRole() {
+    console.log("Querying: Updating employee role");
+    mainMenu();
+}
+
+//queries to view roles
+function qGetRoles(cb) {
+    console.log("Querying all roles");
+    cb();
+}
+
+//queries to add new role
+function qAddRole() {
+    console.log("Querying: add new role");
+    mainMenu();
+}
+
+//queries all departments
+function qGetDepartments() {
+    console.log("Querying all departments");
+    displayDepartments();
+}
+
+//Queries to add a new department
+function qAddDepartment() {
+    console.log("Querying: add department");
+    mainMenu();
+}
+
+
+//PROMPTS ====================================
+
+//Ask user for information of the new employee to add
+function promptForEmployeeinfo() {
+    console.log("Prompting employee info to add");
+    qAddEmployee();
+
+}
+
+//Ask user for informatino of the new department to add
+function promptDepartmentInfo() {
+    console.log("Prompting employee info to add");
+    qAddDepartment();
+
+}
+
+//Ask user for information of the new role to add
+function promptRoleInfo() {
+    console.log("Prompting for new role informatin to add");
+    qAddRole()
+}
+
+//Asks user to select an employee
+function promptSelectEmployees() {
+    console.log("prompting to select an employee...");
+    qGetRoles(promptUpdateRole);
+}
+//Asks user to choose a new role to update the employee role
+function promptUpdateRole() {
+    console.log("Prompting to select a new employee role...");
+    qUpdateEmployeeRole();
+
+}
+
+//DISPLAY ==========================================
+
+//displays employees
+function displayEmployees() {
+    console.log("Displaying Employees...");
+    mainMenu();
+}
+
+//displays roles
+function displayRoles() {
+    console.log("Displaying Roles...");
+    mainMenu();
+}
+
+//displays department
+function displayDepartments() {
+    console.log("Displaying Departments...");
+    mainMenu();
+}
+
+
+
