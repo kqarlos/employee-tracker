fe541bf0d7fcddb142bcf4d2cf2eb51ff0bd18a5
kqarlos <kqarlos@hotmail.com>
2020-01-17 05:52:37 -0800

    Adds functionality to remove employee. Adds comments

--- employeeTracker.js
+++ employeeTracker.js
@@ -10,7 +10,7 @@ var menu = {
         // "View All Employees by Department",
         // "View All Employees by Manager",
         "Add Employee",
-        // "Remove Employee",
+        "Remove Employee",
         "Update Employee Role",
         // "Update Employee Manager",
         "View All Roles",
@@ -44,7 +44,7 @@ function mainMenu() {
     inquirer.prompt(menu).then(function (res) {
         switch (res.menuChoice) {
             case "View All Employees":
-                qGetEmployees(displayEmployees);
+                displayEmployees();
                 break;
             case "View All Employees by Department":
                 break;
@@ -54,9 +54,10 @@ function mainMenu() {
                 promptForEmployeeinfo();
                 break;
             case "Remove Employee":
+                removeEmployee();
                 break;
             case "Update Employee Role":
-                qGetEmployees(promptSelectEmployees);
+                updateEmployeeRole();
                 break;
             case "Update Employee Manager":
                 break;
@@ -86,18 +87,43 @@ function mainMenu() {
     });
 }
 
-//QUERIES ===================================
+//Calls to get employees and roles. Calls to prompt user to select an employee.
+//Calls to prompt user to select a  role to update the selected employee's role
+function updateEmployeeRole() {
+    qGetEmployees().then(function (employees) {
+        qGetRoles().then(function (roles) {
+            promptSelectEmployees(employees).then(function (employeeid) {
+                promptUpdateRole(employeeid, roles);
+            });
+        });
+    });
+}
+
+//Calls to get all employees and to prompt user to select an employee. 
+//Calls to remove employee based on the user's employee choice
+function removeEmployee() {
+    qGetEmployees().then(function (employees) {
+        promptSelectEmployees(employees).then(function (employeeid) {
+            qRemoveEmployee(employeeid);
+        });
+    })
+}
+
+//==================================== QUERIES ===================================
 
 //queries all employees
 function qGetEmployees(cb) {
     console.log("Querying all employees...");
-    connection.query("SELECT * FROM Employee", function (err, res) {
-        if (err) throw err;
-        cb(res);
+    return new Promise(function (resolve, reject) {
+        connection.query("SELECT * FROM Employee", function (err, res) {
+            if (err) return reject(Error(err));;
+            resolve(res);
+        });
     });
 }
 
 //queries to add employee
+//@param {array} employee - array of new employees first name, last name and roleid
 function qAddEmployee(employee) {
     console.log("Querying: add employee...");
     connection.query("INSERT INTO employee(first_name, last_name, role_id) VALUES (?, ?, ?)", employee, function (err, res) {
@@ -106,7 +132,19 @@ function qAddEmployee(employee) {
     });
 }
 
+//queries to remove employee 
+//@param employeeid - employee's id to remove
+function qRemoveEmployee(employeeid) {
+    console.log("Querying: removing employee...");
+    connection.query("DELETE FROM employee WHERE id=?", employeeid, function (err, res) {
+        if (err) throw err;
+        mainMenu();
+    });
+}
+
 //queries to update employee role
+//@param employeedid - id of employee to update role to
+//@param roleid - id of the employee's new role
 function qUpdateEmployeeRole(employeeid, roleid) {
     console.log("Querying: Updating employee role");
     connection.query("UPDATE employee SET role_id = ? WHERE id = ?", [roleid, employeeid], function (err, res) {
@@ -115,7 +153,7 @@ function qUpdateEmployeeRole(employeeid, roleid) {
     });
 }
 
-//queries to view roles
+//queries and returns roles
 function qGetRoles() {
     console.log("Querying all roles");
     return new Promise(function (resolve, reject) {
@@ -127,6 +165,7 @@ function qGetRoles() {
 }
 
 //queries to add new role
+//@param role - array of the new role's title, salary and department id
 function qAddRole(role) {
     console.log("Querying: add new role");
     connection.query("INSERT INTO role(title, salary, department_id) VALUES (?, ?, ?)", role, function (err, res) {
@@ -135,7 +174,7 @@ function qAddRole(role) {
     });
 }
 
-//queries all departments
+//queries and returns all departments
 function qGetDepartments() {
     console.log("Querying all departments");
     return new Promise(function (resolve, reject) {
@@ -147,6 +186,7 @@ function qGetDepartments() {
 }
 
 //Queries to add a new department
+//@param department - new department's name
 function qAddDepartment(department) {
     console.log("Querying: add department");
     connection.query("INSERT INTO department(name) VALUES (?)", department, function (err, res) {
@@ -156,9 +196,11 @@ function qAddDepartment(department) {
 }
 
 
-//PROMPTS ===============================================
+//======================================== PROMPTS =================================================
 
 //Ask user for information of the new employee to add
+//Gets all roles titles to let the user choose new employee's role
+//calls to query add employee
 function promptForEmployeeinfo() {
     console.log("Prompting employee info to add");
     qGetRoles().then(function (roles) {
@@ -200,7 +242,7 @@ function promptForEmployeeinfo() {
     });
 }
 
-//Ask user for informatino of the new department to add
+//Ask user for information of the new department to add and calls to query add department
 function promptDepartmentInfo() {
     console.log("Prompting department info to add");
 
@@ -215,12 +257,11 @@ function promptDepartmentInfo() {
             res.name
         ]);
     });
-
-
-
 }
 
 //Ask user for information of the new role to add
+//Gets department names to let the user choose the role department
+//Calls to query add role
 function promptRoleInfo() {
     console.log("Prompting for new role informatin to add");
     qGetDepartments().then(function (departments) {
@@ -257,38 +298,36 @@ function promptRoleInfo() {
                 departmentid
             ]);
         });
-
-
-
     });
 }
 
-//Asks user to select an employee
+//Asks user to select an employee by getting list of employee names
+//This resolves once the user selects an employee name and this name is mapped to the employee id.
+//@param employees - list of objects with employee information
 function promptSelectEmployees(employees) {
     console.log("prompting to select an employee...");
-    let names = employees.map(e => {
-        return (e.first_name);
-    });
-    console.log(names);
-    inquirer.prompt({
-        type: "list",
-        name: "employeeName",
-        message: "Select an employee",
-        choices: names
-    }).then(function (res) {
-        var employeeid;
-        employees.forEach(e => {
-            if (e.first_name === res.employeeName) {
-                employeeid = e.id;
-            }
+    return new Promise(function (resolve, reject) {
+        if (!employees) return reject(Error("No employees found!"));
+        let names = employees.map(e => {
+            return (e.first_name);
         });
-        qGetRoles().then(function (roles) {
-            promptUpdateRole(employeeid, roles);
+        inquirer.prompt({
+            type: "list",
+            name: "employeeName",
+            message: "Select an employee",
+            choices: names
+        }).then(function (res) {
+            employees.forEach(e => {
+                if (e.first_name === res.employeeName) {
+                    resolve(e.id);
+                }
+            });
         });
     });
 }
 
-//Asks user to choose a new role to update the employee role
+//Asks user to choose a new role to update the employee
+//Calls to query update employee
 function promptUpdateRole(employeeid, roles) {
     let roleTitles = roles.map(r => {
         return (r.title);
@@ -306,19 +345,19 @@ function promptUpdateRole(employeeid, roles) {
                 roleid = r.id;
             }
         });
-
         qUpdateEmployeeRole(employeeid, roleid);
     });
-
 }
 
 //DISPLAY ==========================================
 
 //displays employees
-function displayEmployees(employees) {
+function displayEmployees() {
     console.log("=========================== Employees ===========================");
-    console.table(employees);
-    mainMenu();
+    qGetEmployees().then(function (res) {
+        console.table(res);
+        mainMenu();
+    });
 }
 
 //displays roles
