6ab940dbc3e6727d22650d03cff9fbe7b7157051
kqarlos <kqarlos@hotmail.com>
2020-11-11 03:39:43 -0800

    Cleans up seed.sql

--- assets/sql/seed.sql
+++ assets/sql/seed.sql
@@ -32,57 +32,33 @@ CREATE TABLE Employee
 
 
 
-INSERT INTO Department (name)
-VALUES("Sales");
-INSERT INTO Department (name)
-VALUES("Finance");
-INSERT INTO Department (name)
-VALUES("Engineering");
-INSERT INTO Department (name)
-VALUES("Legal");
+INSERT INTO Department (name) VALUES
+("Sales"), ("Finance"), ("Engineering"), ("Legal");
 
-INSERT INTO Role (title, salary, department_id)
-VALUES("Salesperson", 80000, 1);
-INSERT INTO Role (title, salary, department_id)
-VALUES("Sales Lead", 10000, 1);
-INSERT INTO Role (title, salary, department_id)
-VALUES("Lead Engineering", 150000, 3);
-INSERT INTO Role (title, salary, department_id)
-VALUES("Software Engineering", 120000, 3);
-INSERT INTO Role (title, salary, department_id)
-VALUES("Accountant", 125000, 2);
-INSERT INTO Role (title, salary, department_id)
-VALUES("Lawyer", 190000, 4);
-INSERT INTO Role (title, salary, department_id)
-VALUES("Legal Team Lead", 250000, 4);
+INSERT INTO Role (title, salary, department_id) VALUES
+("Salesperson", 80000, 1),
+("Sales Lead", 10000, 1),
+("Lead Engineering", 150000, 3),
+("Software Engineering", 120000, 3),
+("Accountant", 125000, 2),
+("Lawyer", 190000, 4),
+("Legal Team Lead", 250000, 4);
 
-INSERT INTO Employee (first_name, last_name, role_id)
-VALUES("Joyce","Thomas",7);
-INSERT INTO Employee (first_name, last_name, role_id)
-VALUES("Wanda","Long",3);
-INSERT INTO Employee (first_name, last_name, role_id)
-VALUES("Paula","Martin",3);
-INSERT INTO Employee (first_name, last_name, role_id)
-VALUES("Brenda","Nelson",2);
-INSERT INTO Employee (first_name, last_name, role_id)
-VALUES("Elizabeth","Walker",5);
-INSERT INTO Employee (first_name, last_name, role_id, manager_id)
-VALUES("Roger","Baker",1,2);
-INSERT INTO Employee (first_name, last_name, role_id, manager_id)
-VALUES("Joan","James",1,2);
-INSERT INTO Employee (first_name, last_name, role_id, manager_id)
-VALUES("Aaron","Russel",4,7);
-INSERT INTO Employee (first_name, last_name, role_id, manager_id)
-VALUES("Louise","Flores",1,2);
-INSERT INTO Employee (first_name, last_name, role_id, manager_id)
-VALUES("Robert","Wilson",4,7);
-INSERT INTO Employee (first_name, last_name, role_id, manager_id)
-VALUES("Lawrence","Ward",6,7);
-INSERT INTO Employee (first_name, last_name, role_id, manager_id)
-VALUES("Teresa","Bennet",6,7);
-INSERT INTO Employee (first_name, last_name, role_id, manager_id)
-VALUES("David","Ross",4,11);
-INSERT INTO Employee (first_name, last_name, role_id, manager_id)
-VALUES("Walter","Perry",4,11);
-INSERT INTO Employee (first_name, last_name, role_id, manager_id)
-VALUES("Evelyn","Diaz",4,11);
+INSERT INTO Employee (first_name, last_name, role_id) VALUES
+("Joyce","Thomas",7),
+("Wanda","Long",3),
+("Paula","Martin",3),
+("Brenda","Nelson",2),
+("Elizabeth","Walker",5);
+
+INSERT INTO Employee (first_name, last_name, role_id, manager_id) VALUES
+("Roger","Baker",1,2),
+("Joan","James",1,2),
+("Aaron","Russel",4,7),
+("Louise","Flores",1,2),
+("Robert","Wilson",4,7),
+("Lawrence","Ward",6,7),
+("Teresa","Bennet",6,7),
+("David","Ross",4,11),
+("Walter","Perry",4,11),
+("Evelyn","Diaz",4,11);
