eff74b60d70ccd9b4417db88ab6d03e1627e5878
kqarlos <kqarlos@hotmail.com>
2021-03-18 20:04:54 -0700

    updates schema

--- assets/sql/schema.sql
+++ assets/sql/schema.sql
@@ -4,29 +4,26 @@ USE etracker_DB;
 
 CREATE TABLE Department
 (
-    id INT NOT NULL AUTO_INCREMENT,
+    id INT AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(30) NOT NULL,
-    PRIMARY KEY (id)
 );
 
 CREATE TABLE Role 
 (
-    id INT NOT NULL AUTO_INCREMENT,
+    id INT AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(30) NOT NULL,
     salary DECIMAL(10,2) default 0,
     department_id INT,
-	PRIMARY KEY (id),
-    FOREIGN KEY (department_id) REFERENCES Department(id)
+    FOREIGN KEY (department_id) REFERENCES Department(id) ON DELETE CASCADE
 );
 
 CREATE TABLE Employee
 (
-    id INT NOT NULL AUTO_INCREMENT,
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
     role_id INT,
     manager_id INT,
-    PRIMARY KEY (id),
-	FOREIGN KEY (role_id) REFERENCES Role(id),
-    FOREIGN KEY (manager_id) REFERENCES Employee(id)
+	FOREIGN KEY (role_id) REFERENCES Role(id) ON DELETE CASCADE,
+    FOREIGN KEY (manager_id) REFERENCES Employee(id) ON DELETE SET NULL
 );
