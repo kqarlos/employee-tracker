76b80f37386ef3754e8f15906de7ae793821502f
kqarlos <kqarlos@hotmail.com>
2021-03-23 18:41:23 -0700

    Updates schema

--- assets/sql/schema.sql
+++ assets/sql/schema.sql
@@ -1,13 +1,16 @@
 DROP DATABASE IF EXISTS etracker_DB;
 CREATE DATABASE etracker_DB;
 USE etracker_DB;
+DROP TABLE IF EXISTS Department;
+DROP TABLE IF EXISTS Role;
+DROP TABLE IF EXISTS Employee;
 
 CREATE TABLE Department
 (
     id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(30) NOT NULL,
     CONSTRAINT UC_name UNIQUE(name)
-);
+)
 
 CREATE TABLE Role 
 (
@@ -16,7 +19,7 @@ CREATE TABLE Role
     salary DECIMAL(10,2) default 0,
     department_id INT UNSIGNED,
     CONSTRAINT FK_department FOREIGN KEY (department_id) REFERENCES Department(id) ON DELETE CASCADE
-);
+)
 
 CREATE TABLE Employee
 (
@@ -27,4 +30,4 @@ CREATE TABLE Employee
     CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES Role(id) ON DELETE CASCADE,
     manager_id INT UNSIGNED,
     CONSTRAINT FK_manager FOREIGN KEY (manager_id) REFERENCES Employee(id) ON DELETE SET NULL
-);
+)
